014184a39b2e65165dc3d3e97d410ff65f68fcd7
gaukaj4 <49602762+gaukaj4@users.noreply.github.com>
2021-01-26 20:36:02 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,42 +1,14 @@
-import React, { Component } from 'react';
-import fire from './config/fire'
-import Home from './Home.js';
-import Login from './Login.js';
-
 import './App.css';
+import LineChart from './components/LineChart';
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    };
-
-    this.authListener = this.authListener.bind(this);
-  }
-
-  componentDidMount() {
-    this.authListener();
-  }
-
-  authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user });
-      } else {
-        this.setState({ user: null });
-      }
-    }) 
-  }
-
-  render() {
-    return (
-      <div className="App">
-        { this.state.user ? ( <Login /> ) : ( <Home /> ) }
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <div className='chart'>
+        <LineChart />        
+        </div>
+    </div>
+  );
 }
 
 export default App;
